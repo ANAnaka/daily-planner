bdd0583e539dc0082bb1880314bda766add120c5
ANAnaka <112645412+ANAnaka@users.noreply.github.com>
2023-02-17 21:11:04 +0300

    Add files via upload

---
+++ Я календарь переверну/Я календарь переверну/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static Я_календарь_переверну.json;
+
+namespace Я_календарь_переверну
+{
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            data.SelectedDate = DateTime.Now;
+           
+        } 
+/*        public void z()
+        {
+                string jsonchik = JsonConvert.SerializeObject(Zametki);
+                File.WriteAllText("C:\\Рабочий стол\\Soxran", jsonchik);
+        
+        }*/
+
+        private void Cozdanie_Click(object sender, RoutedEventArgs e)
+        {
+            Nazvanie
+        }
+    }
+}
---
+++ Я календарь переверну/Я календарь переверну/json.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Я_календарь_переверну
+{
+    internal class json
+    {   internal class Zametki
+        {
+            List<Zametki> zam = new List<Zametki>();
+            public string nazvanie;
+            public string opisanie;
+
+        }
+
+    }
+
+
+}
